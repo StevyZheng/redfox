e166652cd9cde031d860ac7e64e585c514429da3
664553716@qq.com <jqy951753>
2018-10-30 18:01:16 +0800

    init

---
+++ rflib/BaseLogging.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# @Author  : Stevy
+import sys
+import os
+import json
+import logging
+import traceback
+import signal
+import atexit
---
+++ rflib/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# @Author  : Stevy
+
+__version__ = "0.0.1"
